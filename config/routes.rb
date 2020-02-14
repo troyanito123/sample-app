18ddc31839ed8e42417c7b40f5e9e144cad54643
jassmanni <jassmanniq@gmail.com>
2020-02-14 12:16:11 -0400

    registro de usuarios listo

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource 'users'
+
 end
