b9e6b4daf6ef442bea97f00bf628746e8c8df0ae
jassmanni <jassmanniq@gmail.com>
2020-02-14 14:09:33 -0400

    helper sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  include SessionsHelper
+
+  before_action :logged_in_user
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = I18n.t 'restricted_page'
+      redirect_to login_path
+    end
+  end
+
 end
