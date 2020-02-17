0dffd29d628985e1f11b924e7fdda85c1db36f96
jassmanni <jassmanniq@gmail.com>
2020-02-17 09:04:32 -0400

    crud comment ready

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: {minimum: 2, maximum: 255}
   validates :body, presence: true, length: {minimum: 2, maximum: 2550}
